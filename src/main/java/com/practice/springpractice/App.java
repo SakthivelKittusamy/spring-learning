a98aa12e4d0d1f71b226c116a2111fac45c35d99
Sakthivel Kittusamy <1994sakthi@gmail.com>
2024-09-28 19:57:38 +0530

    initial commit

--- src/main/java/com/practice/springpractice/App.java
+++ src/main/java/com/practice/springpractice/App.java
@@ -1,12 +1,7 @@
 package com.practice.springpractice;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext ac = new ClassPathXmlApplicationContext("spring.xml");
-		Developer dev = (Developer) ac.getBean("developer");
-		dev.develop();
+		System.out.println("Welcome!!!");
 	}
 }
